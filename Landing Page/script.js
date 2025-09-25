98a424e15487f739189ce5dd505ad17ebd4f6783
Santhiya.K <santhiyak2003k@gmail.com>
2025-09-25 20:17:56 +0530

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,15 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Optional: Close the menu when a link is clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        if (navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+        }
+    });
+});
